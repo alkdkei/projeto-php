c6e9690855abf789891a9bc88a5ee12593d9f15f
alkdkei <marcos.ripcurl3@gmail.com>
2018-06-21 21:08:46 -0300

    inicio Projeto PHP

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+	$titulo =
+	$tituloConteudo = "<h1>título de texto</h1>";
+	$conteudo = "<p>GIT - Versionamento</p>";
+?>
+<html>
+<head>
+	<title><?php echo $titulo; ?></title>
+	<meta charset="utf-8" />
+</head>
+<body>
+<?php
+	echo $tituloConteudo;
+	echo $conteudo;
+?>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+</html>
